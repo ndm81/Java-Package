02a240ab1052e6fabe8610679529e27192364a60
Nadeem012 <55021916+ndm81@users.noreply.github.com>
2020-11-08 11:45:48 +0530

    Add files via upload

---
+++ package.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main{
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Enter the name");
+
+		String myName=obj.nextLine();
+		System.out.println("My name is "+myName);
+
+	}
+}
